ee1c43406d41cd43b078875c4e4a85b66577e849
AshShareef.Tariq.19 <TaAsh19@icstudents.org>
2017-11-13 15:29:12 -0600

    Added Stack interface

---
+++ 2nd Tri/U12A1/Stack.java
@@ -0,0 +1,17 @@
+// Tariq
+// This interface will set up a Stack.
+
+public interface Stack
+{
+	boolean isEmpty();
+	// return true if stack has no elements, false otherwise.
+
+	void push(Object obj);
+	// pushes an elemnt onto the stack.
+
+	Object pop();
+	// pops an element off the stack and returns it.
+
+	Object peekTop();
+	// returns the element on top of the stack without removing it
+}
