9ba9168f2f70a13fb3d2a7e3ffe072cb09d9902c
fastyfoxy <fastyfoxy@gmail.com>
2017-11-14 00:40:18 -0600

    Added U12A1

---
+++ 2nd Tri/U12A1/U12A1.java
@@ -0,0 +1,22 @@
+// Tariq
+//
+// This program will test the methods of Stack, and ArrayStack.
+
+import java.util.StringTokenizer;
+
+public class U12A1
+{
+	private ArrayStack stack;
+	String exp1 = "5 2 + 8 5 - *";
+	String exp2 = "4 7 * 4 9 - /";
+
+	public static void main(String[] args)
+	{
+		U12A1 app = new U12A1();
+	}
+
+	public U12A1()
+	{
+
+	}
+}
