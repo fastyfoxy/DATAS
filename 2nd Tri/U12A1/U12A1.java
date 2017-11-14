62d77a946b45640c425b83710683780856ae48fe
AshShareef.Tariq.19 <TaAsh19@icstudents.org>
2017-11-14 11:30:52 -0600

    Updated ArrayStack & .gitignore

--- 2nd Tri/U12A1/U12A1.java
+++ 2nd Tri/U12A1/U12A1.java
@@ -3,10 +3,12 @@
 // This program will test the methods of Stack, and ArrayStack.
 
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class U12A1
 {
-	private ArrayStack stack;
+	private ArrayStack stack1;
+	private ArrayStack stack2;
 	String exp1 = "5 2 + 8 5 - *";
 	String exp2 = "4 7 * 4 9 - /";
 
@@ -17,6 +19,21 @@ public class U12A1
 
 	public U12A1()
 	{
+		evaluate(stack1, exp1);
+	}
 
+	public void evaluate(ArrayStack stack, String exp)
+	{
+		StringTokenizer st = new StringTokenizer(exp);
+
+		while (st.hasMoreTokens())
+		{
+			if(st.nextToken().equals("+") || st.nextToken().equals("-") || st.nextToken().equals("*") ||st.nextToken().equals("/"))
+				stack.push(st.nextToken());
+			else
+			{
+
+			}
+		}
 	}
 }
