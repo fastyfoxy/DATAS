51b784a7d9eb2d7682b8c9ce12341236ea6890da
fastyfoxy <fastyfoxy@gmail.com>
2017-11-16 23:04:08 -0600

    Updated U12A1.java

--- 2nd Tri/U12A1/U12A1.java
+++ 2nd Tri/U12A1/U12A1.java
@@ -2,13 +2,15 @@
 //
 // This program will test the methods of Stack, and ArrayStack.
 
+//TODO: solve double problem, is currently optimized for int
+
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 
 public class U12A1
 {
-	private ArrayStack stack1;
-	private ArrayStack stack2;
+	private ArrayStack stack1 = new ArrayStack();
+	private ArrayStack stack2 = new ArrayStack();
 	String exp1 = "5 2 + 8 5 - *";
 	String exp2 = "4 7 * 4 9 - /";
 
@@ -20,6 +22,7 @@ public class U12A1
 	public U12A1()
 	{
 		evaluate(stack1, exp1);
+		evaluate(stack2, exp2);
 	}
 
 	public void evaluate(ArrayStack arraystack, String str)
@@ -31,24 +34,24 @@ public class U12A1
 			String s = st.nextToken();
 
 			if(isInteger(s))
-				arraystack.push(s);
+				arraystack.push(Integer.valueOf(s));
 			else
 			{
 				Integer operand1 = (Integer) arraystack.pop();
 				Integer operand2 = (Integer) arraystack.pop();
 
-				if(st.nextToken().equals("+"))
+				if(s.equals("+"))
 					arraystack.push(operand2+operand1);
-				if(st.nextToken().equals("-"))
+				if(s.equals("-"))
 					arraystack.push(operand2-operand1);
-				if(st.nextToken().equals("*"))
+				if(s.equals("*"))
 					arraystack.push(operand2*operand1);
-				if(st.nextToken().equals("/"))
+				if(s.equals("/"))
 					arraystack.push(operand2/operand1);
 			}
 		}
 
-		System.out.printf("%.1f", arraystack.peekTop());
+		System.out.printf("The Answer is %.1f",(double)((Integer)arraystack.peekTop()).intValue());
 		System.out.println();
 	}
 
