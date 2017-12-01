d98594e5ee239293334f11e3a32783a7b97b21f9
fastyfoxy <fastyfoxy@gmail.com>
2017-11-30 21:55:44 -0600

    added u12a2

---
+++ 2nd Tri/U12A2/U12A2.java
@@ -0,0 +1,14 @@
+// This program will test the methods of MazeSolver.
+
+public class U12A2
+{
+	public static void main(String[] args)
+	{
+		U12A2 app = new U12A2();
+	}
+
+	public U12A2()
+	{
+
+	}
+}
