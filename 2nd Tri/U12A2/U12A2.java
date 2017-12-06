cd42819d1aeda77ca193d71e5a16632e249fd60d
fastyfoxy <fastyfoxy@gmail.com>
2017-12-06 09:04:14 -0600

    Update U12A2.java

--- 2nd Tri/U12A2/U12A2.java
+++ 2nd Tri/U12A2/U12A2.java
@@ -1,7 +1,12 @@
-// This program will test the methods of MazeSolver.
+// This program will solve and exit a maze.
+
+import java.util.Scanner;
+import java.io.*;
 
 public class U12A2
 {
+	private ArrayStack stack = new ArrayStack();
+
 	public static void main(String[] args)
 	{
 		U12A2 app = new U12A2();
@@ -9,6 +14,56 @@ public class U12A2
 
 	public U12A2()
 	{
-		MazeSolver solver = new MazeSolver();
+		try
+		{
+			String[] temp = new String[10];
+			int c;
+			
+			Scanner in = new Scanner(new File("prg1071a.txt"));
+			while(in.hasNext())
+			{
+				temp[c] = in.nextLine();
+				c++;
+			}
+			
+			for(int i=0; i<10; i++)
+				stack.push(temp.pop());
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e.toString());
+		}
+
+		printStack(xPos, yPos);
+		if(findRoute())
+			printRoute();
+		else
+			failed();
+	}
+
+	public void printStack(ArrayStack stack)
+	{
+		System.out.println("Maze:");
+		
+		for(int i=0; i<0; i++)
+			System.out.println(stack.pop());
+
+		System.out.println();
+		System.out.println("Starting position: ("+x+", "+y+")");
+	}
+
+	public boolean findRoute()
+	{
+		return false;
+	}
+
+	public void failed()
+	{
+
+	}
+
+	public void printRoute()
+	{
+
 	}
 }
