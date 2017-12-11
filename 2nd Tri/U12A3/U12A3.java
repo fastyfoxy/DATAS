7413c059ab43f7a3c763d89e830f2cee648c04c1
fastyfoxy <fastyfoxy@gmail.com>
2017-12-11 00:03:07 -0600

    added u12a3

---
+++ 2nd Tri/U12A3/U12A3.java
@@ -0,0 +1,16 @@
+// This program will simulate the operation of a car wash for 10 hours.
+
+public class U12A3
+{
+	private ListQueue queue;
+
+	private static void main(String[] args)
+	{
+		U12A3 app = new U12A3();
+	}
+
+	public U12A3()
+	{
+
+	}
+}
