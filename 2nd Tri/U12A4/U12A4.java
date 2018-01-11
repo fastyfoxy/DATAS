a3653047eb4102e6b4936f8ff1bd744f2e645234
fastyfoxy <fastyfoxy@gmail.com>
2018-01-11 00:16:45 -0600

    a

--- 2nd Tri/U12A4/U12A4.java
+++ 2nd Tri/U12A4/U12A4.java
@@ -1,12 +1,39 @@
-// This program will
+// This program will distribute processing priority.
+
+import java.util.Scanner;
+import java.io.*;
 
 public class U12A4
 {
-	private LinkedList list;
+	private ListQueue queue = new ListQueue();
+
+	public static void main(String[] args)
+	{
+		U12A4 app = new U12A4();
+	}
 
 	public U12A4()
 	{
-		list = new LinkedList();
-	}
+		try
+		{
+			Scanner in = new Scanner(new File("prg1060z.txt"));
 
+			for(int c=0; in.hasNext(); c++)
+			{
+				String line = in.nextLine();
+
+				for(int i=0; i<10; i++)
+					queue.enqueue(line);
+			}
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e.toString());
+		}
+
+		for(int i=0; i<queue.size(); i++)
+		{
+
+		}
+	}
 }
