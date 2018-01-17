c4452ce035fafc43524cfc0658b171a0323ba000
fastyfoxy <fastyfoxy@gmail.com>
2018-01-17 16:01:42 -0600

    Add files via upload

---
+++ 2nd Tri/U12A5/U12A5.java
@@ -0,0 +1,28 @@
+// This program will simulate a large computer system.
+
+import java.util.ListIterator;
+
+public class U12A5
+{
+	private ListQueue queue = new ListQueue();
+
+	public static void main(String[] args)
+	{
+		U12A5 app = new U12A5();
+	}
+
+	public U12A5()
+	{
+	}
+
+	public void a()
+	{
+	}
+}
+
+/*
+OUTPUT:
+
+///
+
+*/
