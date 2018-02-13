97161bf0430e8997216ee560942e470c4913210b
fastyfoxy <fastyfoxy@gmail.com>
2018-02-13 00:33:31 -0600

    Update U12A5.java

--- 2nd Tri/U12A5/U12A5.java
+++ 2nd Tri/U12A5/U12A5.java
@@ -1,5 +1,7 @@
-// This program will simulate a large computer system.
+// This program will act as a waiting line for print operations.
 
+import java.util.Scanner;
+import java.util.LinkedList;
 import java.util.ListIterator;
 
 public class U12A5
@@ -13,16 +15,127 @@ public class U12A5
 
 	public U12A5()
 	{
+		Scanner in = new Scanner(System.in);
+		String cmd = "";
+		int num = 0;
+
+		for(;;)
+		{
+			System.out.print("Enter task: A)dd, D)elete, E)xit: ");
+			cmd = in.nextLine();
+
+			if(cmd.equalsIgnoreCase("A"))
+			{
+				System.out.print("Enter number: ");
+				queue.enqueue(Integer.parseInt(in.nextLine()));
+			}
+			else if(cmd.equalsIgnoreCase("D"))
+			{
+				System.out.print("Enter number: ");
+				del(Integer.parseInt(in.nextLine()));
+			}
+			else if(cmd.equalsIgnoreCase("E"))
+			{
+				exit();
+			}
+			else
+				break;
+
+			System.out.println("This is the list");
+			printQueue();
+		}
 	}
 
-	public void a()
+	public void del(int n)
 	{
+		LinkedList list = queue.getList();
+		ListIterator<Integer> iter = list.listIterator();
+
+		while(iter.hasNext())
+			if(n == iter.next())
+			{
+				iter.remove();
+				return;
+			}
+
+		System.out.println("Number does not exist");
+		return;
+	}
+
+	public void printQueue()
+	{
+		LinkedList list = queue.getList();
+		ListIterator<Integer> iter = list.listIterator();
+
+		while(iter.hasNext())
+		{
+			System.out.print(iter.next()+" ");			// list prints and dequeues
+		}
+
+		System.out.println("\n");
+	}
+
+	public void exit()
+	{
+		System.exit(0);
 	}
 }
 
 /*
 OUTPUT:
 
-///
+Enter task: A)dd, D)elete, E)xit: A
+Enter number: 10
+This is the list
+10
+
+Enter task: A)dd, D)elete, E)xit: A
+Enter number: 15
+This is the list
+10 15
+
+Enter task: A)dd, D)elete, E)xit: A
+Enter number: 2
+This is the list
+10 15 2
+
+Enter task: A)dd, D)elete, E)xit: D
+Enter number: 10
+This is the list
+15 2
+
+Enter task: A)dd, D)elete, E)xit: D
+Enter number: 2
+This is the list
+15
+
+Enter task: A)dd, D)elete, E)xit: D
+Enter number: 15
+This is the list
+
+
+Enter task: A)dd, D)elete, E)xit: A
+Enter number: 7
+This is the list
+7
+
+Enter task: A)dd, D)elete, E)xit: D
+Enter number: 8
+Number does not exist
+This is the list
+7
+
+Enter task: A)dd, D)elete, E)xit: A
+Enter number: 12
+This is the list
+7 12
+
+Enter task: A)dd, D)elete, E)xit: D
+Enter number: 12
+This is the list
+7
+
+Enter task: A)dd, D)elete, E)xit: E
+Press any key to continue . . .
 
 */
