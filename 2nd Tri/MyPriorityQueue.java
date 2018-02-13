f98c30769d525e5bd060213f4e40d61b81d24e66
fastyfoxy <fastyfoxy@gmail.com>
2018-02-13 15:19:05 -0600

    Add files via upload

---
+++ 2nd Tri/MyPriorityQueue.java
@@ -0,0 +1,29 @@
+// This program will run a priority queue of ER patients.
+
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+
+public class MyPriorityQueue
+{
+	private ArrayList<ERPatient> patients = new ArrayList<ERPatient>();
+
+	public MyPriorityQueue()
+	{
+
+	}
+
+	public void add()
+	{
+
+	}
+
+	public void removeMin()
+	{
+
+	}
+
+	public boolean isEmpty()
+	{
+		return patients.isEmpty();
+	}
+}
